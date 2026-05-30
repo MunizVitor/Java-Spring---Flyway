c9d908b4b0363e06def84bd5ec873ffb022be82b
MunizVitor <vmuniz.dev@gmail.com>
2026-05-30 18:56:40 -0300

    criando classe de entidades de product

---
+++ src/main/java/com/springFlyway/model/ProductModel.java
@@ -0,0 +1,32 @@
+package com.springFlyway.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 13)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ean;
+
+    private String nomeProduto;
+    private String descricaoProduto;
+    private BigDecimal precoProduto;
+
+}

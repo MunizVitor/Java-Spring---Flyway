79c8b9d46c3d282d580afd7e355fd85a4015a537
MunizVitor <vmuniz.dev@gmail.com>
2026-05-30 20:38:37 -0300

    finalização de endpoints da API

--- src/main/java/com/springFlyway/controller/ProductController.java
+++ src/main/java/com/springFlyway/controller/ProductController.java
@@ -3,12 +3,10 @@ package com.springFlyway.controller;
 import com.springFlyway.model.ProductModel;
 import com.springFlyway.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("flyway")
@@ -19,7 +17,36 @@ public class ProductController {
 
     @PostMapping("/save")
     public ResponseEntity createProduct(@Validated @RequestBody ProductModel product) {
-        ProductModel productExistente = repository.findBy(product.getNomeProduto());
-
+        var productExistente = repository.findByNomeProduto(product.getNomeProduto());
+        if(productExistente.isPresent()){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Produto já existe");
+        }
+        repository.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Produto criado com sucesso, código do produto: " + product.getEan());
     }
+
+    @GetMapping("products")
+    public ResponseEntity getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+    }
+
+    @GetMapping("products/{codigoProduto}")
+    public ResponseEntity getCodigoProducts(@PathVariable String codigoProduto) {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.findByEan(codigoProduto)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado pelo código: " + codigoProduto)));
+    }
+
+    @GetMapping("products/{descricaoProduto}")
+    public ResponseEntity getDescricaoProducts(@PathVariable String descricaoProduto) {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.findByDescricaoProduto(descricaoProduto)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado pela descrição: " + descricaoProduto)));
+    }
+
+    @GetMapping(value = "products", params = "nomeProduto")
+    public ResponseEntity<?> getNomeProducts(@RequestParam String nomeProduto) {
+        return repository.findByNomeProduto(nomeProduto)
+                .map(product -> ResponseEntity.ok(product))
+                .orElseGet(() -> (ResponseEntity<ProductModel>) ResponseEntity.status(HttpStatus.NOT_FOUND));
+    }
+
 }

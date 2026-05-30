0b4b25ac747ee676542161e0461a3e7f922d9610
MunizVitor <vmuniz.dev@gmail.com>
2026-05-30 19:40:28 -0300

    adicionando funcoes a repository

---
+++ src/main/java/com/springFlyway/repositories/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.springFlyway.repositories;
+
+import com.springFlyway.model.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+    Optional<ProductModel> findByNomeProduto(String nomeProduto);
+    Optional<ProductModel> findByDescricaoProduto(String descricaoProduto);
+    Optional<ProductModel> findByEan(String ean);
+}
